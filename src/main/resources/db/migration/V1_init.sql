bc7636fc36270815e82ac293360323036af5f6dc
Paul Pan <panjunhong05@gmail.com>
2020-01-28 13:47:58 -0800

    Added Thymeleaf and revised SQL schema

--- src/main/resources/db/migration/V1_init.sql
+++ src/main/resources/db/migration/V1_init.sql
@@ -4,6 +4,5 @@ CREATE TABLE custom_url
 (
     id           serial PRIMARY KEY,
     route        TEXT UNIQUE NOT NULL,
-    original_url TEXT        NOT NULL,
-    user_id      serial      NOT NULL
+    original_url TEXT        NOT NULL
 );
