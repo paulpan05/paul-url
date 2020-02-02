2bf5b70dd2477c1f30b9adb0ea6598eb91c56eb8
Paul Pan <panjunhong05@gmail.com>
2020-02-02 14:06:30 -0800

    Added jaxb api

--- src/test/java/paulurl/shortener/URLShortenerAppTests.java
+++ src/test/java/paulurl/shortener/URLShortenerAppTests.java
@@ -3,11 +3,14 @@ package paulurl.shortener;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class URLShortenerAppTests {
 
 	@Test
-	void contextLoads() {
+	void sampleTest() {
+	  assertEquals(200, 200);
 	}
 
 }
