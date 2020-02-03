fe72d50a18ab63f57d970cc13f9cd234629b0143
Paul Pan <panjunhong05@gmail.com>
2020-02-02 22:05:27 -0800

    Temporarily added allow all cors

--- src/main/java/paulurl/shortener/UrlController.java
+++ src/main/java/paulurl/shortener/UrlController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("v1/api")
 public class UrlController {
