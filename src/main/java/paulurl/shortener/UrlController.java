3d27f32448d579cd42c319111e7d4c51ffc5d76d
Paul Pan <panjunhong05@gmail.com>
2020-02-03 02:36:14 -0800

    Allow Cross Origin only from paulurl.com

--- src/main/java/paulurl/shortener/UrlController.java
+++ src/main/java/paulurl/shortener/UrlController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "https://paulurl.com/", maxAge = 3600)
 @RestController
 @RequestMapping("v1/api")
 public class UrlController {
